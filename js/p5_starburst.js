6d7caa75da93afc47a4af07233d8c76ba856520c
Brit <britkos@gmail.com>
2015-12-13 22:39:52 -0700

    updated schedule

--- js/p5_starburst.js
+++ js/p5_starburst.js
@@ -18,10 +18,19 @@ var backRed = 170;
 var backGreen = 136;
 var backBlue = 170;
 
+var btnMargin = 20;
+
 var resetBtnHeight = 30;
 var resetBtnWidth = 110;
-var resetBtnY = canvasHeight-20-30;
-var resetBtnX = 20;
+var resetBtnY = canvasHeight-btnMargin-resetBtnHeight;
+var resetBtnX = btnMargin;
+
+var ee1BtnDiameter = 30;
+var ee1BtnRadius = ee1BtnDiameter / 2;
+var ee1BtnY = canvasHeight-btnMargin-ee1BtnRadius;
+var ee1BtnX = canvasWidth-ee1BtnRadius-btnMargin;
+var ee1Inside = false;
+var ee1On = false;
 
 function setup() {
 	var canvas = createCanvas(canvasWidth, canvasHeight);
@@ -53,21 +62,76 @@ function setup() {
 function draw() {
 	//background(backRed, backGreen, backBlue);
 	for(var i=0; i<starsCount; i++) {
-		if(i < 100) {
-			fill(backRed+stars[i].colorDiff, backGreen+stars[i].colorDiff, backBlue+stars[i].colorDiff);
-		} else
-			//fill(80, 184, 226);
-			fill(200, 184, 226);
-		drawDot(stars[i]);
+		drawDot(stars[i], i);
 	}
 	
-	drawButton();
+	drawButtons();
+};
+
+function drawDot(star, i) {
+	// determine fill color
+	if(i < 100) {
+		fill(backRed+stars[i].colorDiff, backGreen+stars[i].colorDiff, backBlue+stars[i].colorDiff);
+	} else
+		fill(200, 184, 226);
+	
+	// draw dot
+	ellipse(star.x, star.y, star.size, star.size);
+	
+	// if edge, bounce
+	if(star.x <= 10 || star.x >= (canvasWidth-10)) {
+		star.xSpeed = star.xSpeed * (-1);
+	}
+	if(star.y <= 10 || star.y >= (canvasHeight-10)) {
+		star.ySpeed = star.ySpeed * (-1);
+	}
+	
+	// move dot
+	if(ee1On) {
+		star.x += (star.xSpeed + ((Math.random() < 0.5) ? (-1) : 1));
+		star.y += (star.ySpeed + ((Math.random() < 0.5) ? (-1) : 1));
+	} else {
+		star.x += star.xSpeed;
+		star.y += star.ySpeed;
+	}
+};
+
+function drawButtons() {
+	// draw the restart button
+	if(mouseX >= resetBtnX && mouseX <= resetBtnX+resetBtnWidth && mouseY >= resetBtnY && mouseY <= resetBtnY+resetBtnHeight) 
+		fill(backRed-50, backGreen-50, backBlue-50);
+	else
+		fill(backRed-30, backGreen-30, backBlue-30);
+	rect(resetBtnX, resetBtnY, resetBtnWidth, resetBtnHeight);
+	fill(0);
+	text("Reset Animation", resetBtnX+10, resetBtnY+20);
+
+	// draw easter egg 1
+	if(mouseX >= (ee1BtnX-ee1BtnRadius) && mouseX <= (ee1BtnX+ee1BtnRadius) && mouseY >= (ee1BtnY-ee1BtnRadius) && mouseY <= (ee1BtnY+ee1BtnRadius)) {
+		fill(backRed-30, backGreen-30, backBlue-30);
+		ellipse(ee1BtnX, ee1BtnY, ee1BtnDiameter, ee1BtnDiameter);
+		fill(0);
+		text("1", ee1BtnX-2, ee1BtnY+4);
+		ee1Inside = true;
+	} else if (ee1Inside) {
+		ee1Inside = false;
+		fill(backRed+stars[0].colorDiff, backGreen+stars[0].colorDiff, backBlue+stars[0].colorDiff);
+		ellipse(ee1BtnX-1, ee1BtnY-1, ee1BtnDiameter+2, ee1BtnDiameter+2);
+	}
 };
 
 function mousePressed() {
+	// if pressed the mouse reset button
 	if(mouseX >= resetBtnX && mouseX <= resetBtnX+resetBtnWidth && mouseY >= resetBtnY && mouseY <= resetBtnY+resetBtnHeight) {
 		background(backRed, backGreen, backBlue);
-		starsCount = 100;		
+		starsCount = 100;
+		starsMore = 0;
+		
+	// if pressed ee1
+	} else if(mouseX >= (ee1BtnX-ee1BtnRadius) && mouseX <= (ee1BtnX+ee1BtnRadius) && mouseY >= (ee1BtnY-ee1BtnRadius) && mouseY <= (ee1BtnY+ee1BtnRadius)) {
+		ee1On = !ee1On;
+		
+	// add extra line
 	} else {
 		// start:	starsCount = 100   starsMore = 0
 		// max:   	starsCount = 130   starsMore = 30
@@ -99,28 +163,3 @@ function mousePressed() {
 		}
 	}
 };
-
-function drawDot(star) {
-	ellipse(star.x, star.y, star.size, star.size);
-	
-	if(star.x <= 10 || star.x >= (canvasWidth-10)) {
-		star.xSpeed = star.xSpeed * (-1);
-	}
-	if(star.y <= 10 || star.y >= (canvasHeight-10)) {
-		star.ySpeed = star.ySpeed * (-1);
-	}
-	
-	star.x += star.xSpeed;
-	star.y += star.ySpeed;
-};
-
-function drawButton() {
-	if(mouseX >= resetBtnX && mouseX <= resetBtnX+resetBtnWidth && mouseY >= resetBtnY && mouseY <= resetBtnY+resetBtnHeight) 
-		fill(backRed-50, backGreen-50, backBlue-50);
-	else
-		fill(backRed-30, backGreen-30, backBlue-30);
-	rect(resetBtnX, resetBtnY, resetBtnWidth, resetBtnHeight);
-	fill(0);
-	text("Reset Animation", resetBtnX+10, resetBtnY+20);
-	
-}
