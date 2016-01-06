b0b9795da8ef8ba22d9c5e050aee456848b25a18
Brit <britkos@gmail.com>
2016-01-06 02:38:50 -0700

    Removing extra font files, js, css, etc. Resizing images  for web, fixing animation

--- includes/css.php
+++ includes/css.php
@@ -4,15 +4,13 @@ function css($up = false) {
 ?>
 <!-- Favicon -->
 <link rel="shortcut icon" href="<?php echo $p; ?>images/favicon.ico" type="image/x-icon">
-<link rel="icon" href="<?php echo $p; ?>images/favicon.ico" type="image/x-icon">
 
 <!-- Bootstrap Core CSS -->
 <link href="<?php echo $p; ?>plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
 <!-- Custom Fonts -->
-<link href="<?php echo $p; ?>plugins/agency/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="<?php echo $p; ?>plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Gruppo' rel='stylesheet' type='text/css'>
 
 <!-- Custom CSS -->
 <link href="<?php echo $p; ?>css/1140.css" rel="stylesheet" type="text/css" media="all">
