05728912c6d89ff9e6f5c2f65a296201370027e8
Brit <britkos@gmail.com>
2016-01-06 17:10:47 -0700

    quick fix for mlh banner

--- index.php
+++ index.php
@@ -43,14 +43,16 @@
 						<li><a class="page-scroll" href="#sponsors">Sponsors</a></li>
 					</ul>
 				</div>
+				
+				<!-- MLH Trust Badge -->
+				<a id="mlh-trust-badge" href="https://mlh.io/seasons/s2016/events?utm_source=s2016&utm_medium=TrustBadge&utm_campaign=s2016" target="_blank">
+					<img src="https://s3.amazonaws.com/logged-assets/trust-badge/s2016.png" alt="MLH Official - Spring 2016" style="width:100%;" >
+				</a>
 
 			</div>
 		</nav>
 		
-		<!-- MLH Trust Badge -->
-		<a id="mlh-trust-badge" href="https://mlh.io/seasons/s2016/events?utm_source=s2016&utm_medium=TrustBadge&utm_campaign=s2016" target="_blank">
-			<img src="https://s3.amazonaws.com/logged-assets/trust-badge/s2016.png" alt="MLH Official - Spring 2016" style="width:100%;" >
-		</a>
+		
 
 
 
