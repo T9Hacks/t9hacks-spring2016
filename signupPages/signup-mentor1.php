089b9162d4792ec896b0c0059866087c13b3d7aa
Brit <britkos@gmail.com>
2016-02-10 05:18:23 -0700

    closing registration for mentors

--- signupPages/signup-mentor1.php
+++ signupPages/signup-mentor1.php
@@ -50,47 +50,75 @@
 			<div class="row" id="participantTop">
 				<div class="signupWrapper">
 				
-					<div class="row">
-						<div class="column12">
-							<h2>Mentor Sign Up</h2>
-							<p>20-21 February 2016</p>
-						</div>
-					</div>
-					<div class="row">
-						<p class="column12">
-							If you are planning on attending T9Hacks, we kindly ask you to register.  This allows us to keep track of 
-							tickets to make sure that we can provide enough space, food, and supplies for everyone.
-						</p>
-						<p class="column12">
-							We require that mentors be present for at least 2 hours during the hackathon.  For more specific 
-							information about mentorship at T9Hacks, please visit our <a href="../documents/T9Hacks_MentorGuide.pdf" target="_blank">Mentor Guide</a>.
-						</p>
-						<div class="column12">
-							<p>How many people are you registering for?</p>
-						</div>
-					</div>
-					<div class="row">
-						<div class="fieldWrapper column12 colleagues">
-							<div class="fieldRadio">
-								<input class="tgl tgl-flip" id="myself" type="radio" name="friends" value="0" <?php echo ($numFriends == 0 ) ? "checked='checked'" : ""; ?> >
-   								<label class="tgl-btn" data-tg-off="Myself" data-tg-on="Myself" for="myself"></label>
-							</div>
-							<div class="fieldRadio">
-								<input class="tgl tgl-flip" id="friend1" type="radio" name="friends" value="1" <?php echo ($numFriends == 1 ) ? "checked='checked'" : ""; ?> >
-   								<label class="tgl-btn" data-tg-off="Myself and 1 Colleague" data-tg-on="Myself and 1 Colleague" for="friend1"></label>
-							</div>
-							<div class="fieldRadio">
-								<input class="tgl tgl-flip" id="friend2" type="radio" name="friends" value="2"<?php echo ($numFriends == 2 ) ? "checked='checked'" : ""; ?> >
-   								<label class="tgl-btn" data-tg-off="Myself and 2 Colleagues" data-tg-on="Myself and 2 Colleagues" for="friend2"></label>
+					<?php if(false) { ?>
+						<div class="row">
+							<div class="column12">
+								<h2>Mentor Sign Up</h2>
+								<p>20-21 February 2016</p>
 							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="fieldWrapper column12">
-							<a href="../signup.php" class="backBtn"><i class="fa fa-angle-double-left"></i> Back</a>
-							<a href="signup-mentor2.php?n=<?php echo $numFriends; ?>" class="btn btn-l right regBtn" id="mentorRegBtn">Registration &nbsp;<i class="fa fa-arrow-circle-o-right"></i></a>
+						<div class="row">
+							<p class="column12">
+								If you are planning on attending T9Hacks, we kindly ask you to register.  This allows us to keep track of 
+								tickets to make sure that we can provide enough space, food, and supplies for everyone.
+							</p>
+							<p class="column12">
+								We require that mentors be present for at least 2 hours during the hackathon.  For more specific 
+								information about mentorship at T9Hacks, please visit our <a href="../documents/T9Hacks_MentorGuide.pdf" target="_blank">Mentor Guide</a>.
+							</p>
+							<div class="column12">
+								<p>How many people are you registering for?</p>
+							</div>
 						</div>
-					</div>
+						<div class="row">
+							<div class="fieldWrapper column12 colleagues">
+								<div class="fieldRadio">
+									<input class="tgl tgl-flip" id="myself" type="radio" name="friends" value="0" <?php echo ($numFriends == 0 ) ? "checked='checked'" : ""; ?> >
+	   								<label class="tgl-btn" data-tg-off="Myself" data-tg-on="Myself" for="myself"></label>
+								</div>
+								<div class="fieldRadio">
+									<input class="tgl tgl-flip" id="friend1" type="radio" name="friends" value="1" <?php echo ($numFriends == 1 ) ? "checked='checked'" : ""; ?> >
+	   								<label class="tgl-btn" data-tg-off="Myself and 1 Colleague" data-tg-on="Myself and 1 Colleague" for="friend1"></label>
+								</div>
+								<div class="fieldRadio">
+									<input class="tgl tgl-flip" id="friend2" type="radio" name="friends" value="2"<?php echo ($numFriends == 2 ) ? "checked='checked'" : ""; ?> >
+	   								<label class="tgl-btn" data-tg-off="Myself and 2 Colleagues" data-tg-on="Myself and 2 Colleagues" for="friend2"></label>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="fieldWrapper column12">
+								<a href="../signup.php" class="backBtn"><i class="fa fa-angle-double-left"></i> Back</a>
+								<a href="signup-mentor2.php?n=<?php echo $numFriends; ?>" class="btn btn-l right regBtn" id="mentorRegBtn">Registration &nbsp;<i class="fa fa-arrow-circle-o-right"></i></a>
+							</div>
+						</div>
+					<?php } else { ?>
+						<div class="row">
+							<div class="fieldWrapper column12">
+								<p>
+									Due to overwhelming response for mentors, we've had to close sign up early.  If you want to keep 
+									informed about T9Hacks, you can follow us on social media!
+								</p>
+								<div class="socialMediaLinks">
+									<ul class="socialButtons">
+										<li>
+											<a href="https://www.facebook.com/t9hacks/" target="_blank"><i class="fa fa-facebook"></i></a>
+										</li>
+										<li>
+											<a href="https://twitter.com/t9hacks" target="_blank"><i class="fa fa-twitter"></i></a>
+										</li>
+										<li>
+											<a href="https://www.instagram.com/t9hacks" target="_blank"><i class="fa fa-instagram"></i></a>
+										</li>
+									</ul>
+								</div>
+								<br/>
+								<br/>
+								<a href="../" class="btn btn-l"><i class="fa fa-arrow-circle-o-left"></i> &nbsp;Back to Home</a>
+							</div>
+						</div>
+					
+					<?php } ?>
 					
 				</div> <!-- end signupWrapper -->
 			</div> <!-- end mentorSignup -->
